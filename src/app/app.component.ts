f13380117eb4149168cda98f5e5dea8cd2b687b0
alex cesar <thealexcesar@gmail.com>
2024-09-27 16:10:38 -0300

    ✅ test: eslint works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {FaIconComponent} from "@fortawesome/angular-fontawesome";
 
 @Component({
   selector: 'denmark-root',
