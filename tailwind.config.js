fa357bdc5d7c35a8187601151d8cb66aae74de13
alex cesar <thealexcesar@gmail.com>
2024-09-28 09:02:12 -0300

    💅 style: custom  tailwind classes + refactor sass

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,25 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+    extend: {
+      colors: {
+        'denmark-blue': '#004b87',
+        'denmark-dark': '#1a1a1a',
+        'denmark-light': '#f0f4f8',
+        'denmark-link': '#4493f8',
+        'denmark-red': '#c8102e',
+      },
+    },
   },
   plugins: [],
 }
-
