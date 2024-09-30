c08e53d0c0dee4ee195db5360a3ef8075761f26b
alex cesar <thealexcesar@gmail.com>
2024-09-30 17:37:29 -0300

    denmark sections no components

---
+++ src/app/pages/history/history.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'denmark-history',
+  standalone: true,
+  imports: [],
+  templateUrl: './history.component.html',
+  styleUrl: './history.component.sass'
+})
+export class HistoryComponent {
+
+}
