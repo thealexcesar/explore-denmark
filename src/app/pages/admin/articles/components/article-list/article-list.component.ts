76163c989dd8610ce4354254e89d6bc82da574c0
alex cesar <thealexcesar@gmail.com>
2024-10-02 10:27:27 -0300

    feature/reestructure

---
+++ src/app/pages/admin/articles/components/article-list/article-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'denmark-article-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './article-list.component.html',
+  styles: ``
+})
+export class ArticleListComponent {
+
+}
---
+++ src/app/pages/admin/articles/pages/article-detail/article-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'denmark-article-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './article-detail.component.html',
+  styles: ``
+})
+export class ArticleDetailComponent {
+
+}
