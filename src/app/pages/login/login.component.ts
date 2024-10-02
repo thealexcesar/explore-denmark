e4ebba7d892652a08508e28b07e24dde6c1ad2b0
alex cesar <thealexcesar@gmail.com>
2024-10-02 17:08:25 -0300

    refactor

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import {AuthService} from "@services/auth.service";
-import {ActivatedRoute, Router} from "@angular/router";
+import {Component} from '@angular/core';
+import {UserService} from "@services/user.service";
+import {Router} from "@angular/router";
 import {FormsModule} from "@angular/forms";
 import {NgIf} from "@angular/common";
-import {RoleType} from "@models/enums/RoleType";
 import {MaterialModules} from "../../shared/modules/material-modules";
-import {ServerAuthResponse} from "@models/auth/server-auth-status";
-import {MatIcon} from "@angular/material/icon";
+import {UserParams} from "@models/user/user-params";
 
 @Component({
   selector: 'denmark-login',
@@ -15,77 +13,28 @@ import {MatIcon} from "@angular/material/icon";
     FormsModule,
     MaterialModules,
     NgIf,
-    MatIcon
   ],
   templateUrl: './login.component.html',
   styles: ``
 })
 export class LoginComponent {
-  credentials = { email: '', password: '' };
-  firstName: string = '';
-  lastName: string = '';
-  isSignIn: boolean = true;
-  title: string = 'Denmark';
+  user: UserParams = { email: '', password: '', name: '' };
+  isSignIn = true;
+  email: string = '';
+  password: string = '';
 
-  constructor(
-    private auth: AuthService,
-    private router: Router,
-    private route: ActivatedRoute
-  ) {}
+  constructor(private authService: UserService, private router: Router) {}
 
-  OnSubmit() {
-    const { email, password } = this.credentials;
-    this.isSignIn ? this.login(email, password) : this.register(email, password);
-  }
-
-  private login(email: string, password: string): void {
-    this.auth.authenticate('login', { email, password }).subscribe({
-      next: (res: ServerAuthResponse) => {
-        localStorage.setItem("token", res.accessToken);
-        this.redirectAfterLogin();
-      },
-      error: (err) => this.handleError(err)
+  onSubmit(): void {
+    const action = this.isSignIn ? 'sign_in' : 'sign_up';
+    this.authService.authenticate(action, this.user).subscribe({
+      next: () => this.router.navigate(['']),
+      error: (error) => console.error(`${action} error:`, error)
     });
   }
 
-  private register(email: string, password: string): void {
-    if (!this.firstName) {
-      alert("O primeiro nome é obrigatório.");
-      return;
-    }
-
-    const name = { first: this.firstName, last: this.lastName || '' };
-    this.auth.authenticate('register', { email, password, name }).subscribe({
-      next: (res: ServerAuthResponse) => {
-        localStorage.setItem("token", res.accessToken);
-        console.log('Registered User Data:', {
-          email,
-          password,
-          name,
-          avatar: null,
-          isActive: true,
-          role: RoleType.USER
-        });
-        this.redirectAfterLogin();
-      },
-      error: (err) => this.handleError(err)
-    });
-  }
-
-  private redirectAfterLogin(): void {
-    const redirectUrl = this.route.snapshot.queryParamMap.get('stateUrl') || '';
-    this.router.navigateByUrl(redirectUrl).then(r => console.log(r));
-  }
-
-  private handleError(err: any): void {
-    console.warn(err.message);
-    alert('An error occurred: ' + err.message);
-  }
-
-  toggleSignIn(): void {
+  toggleSign(): void {
     this.isSignIn = !this.isSignIn;
-    this.credentials = { email: '', password: '' };
-    this.firstName = '';
-    this.lastName = '';
+    this.isSignIn && this.user.name;
   }
 }
