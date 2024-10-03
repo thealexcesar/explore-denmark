48b28c58676739a5aa0440329990ac5226ddcb0c
alex cesar <thealexcesar@gmail.com>
2024-10-03 17:42:22 -0300

    to presentate

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,9 +5,6 @@ import {DemographyComponent} from "@pages/home/demography/demography.component";
 import {EconomyComponent} from "@pages/home/economy/economy.component";
 import {HistoryComponent} from "@pages/home/history/history.component";
 import {NavbarComponent} from "@components/navbar/navbar.component";
-import {User} from "@models/user/user";
-import {UserService} from "@services/user.service";
-import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'denmark-home',
