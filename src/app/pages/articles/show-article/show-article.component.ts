15ed7bef54b49cc04a3ecab2951cb12a5c568316
alex cesar <thealexcesar@gmail.com>
2024-10-04 19:25:51 -0300

    feature/articles-crud

---
+++ src/app/pages/articles/show-article/show-article.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'denmark-show-article',
+  standalone: true,
+  imports: [],
+  templateUrl: './show-article.component.html',
+  styles: ``
+})
+export class ShowArticleComponent {
+
+}
